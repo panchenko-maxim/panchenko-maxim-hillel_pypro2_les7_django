dc71f1e50dfc7100f0423ff52bffcb2ab3346346
root221 <panchenko.maxim87@gmail.com>
2025-03-13 10:45:49 +0200

    9 lesson, 40 min

---
+++ task_manager/accounts/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, phone_number, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email field required')
+        if not phone_number:
+            raise ValueError('Phone field required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(phone_number, email, password, **extra_fields)
+
+    def active_users_after_date(self, date):
+        return self.filter(is_active=True, date_joined__gte=date)
+
+
+
+
