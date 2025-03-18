e91810ba198150606ddba5810aa978a99b2c7c0d
panchenko-maxim <panchenko.maxim87@google.com>
2025-03-19 01:19:59 +0200

    10 lesson 1:02:22

--- task_manager/permissions/urls.py
+++ task_manager/permissions/urls.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.urls import path
 from permissions.views import list_users, manage_permissions
 
@@ -6,5 +5,4 @@ from permissions.views import list_users, manage_permissions
 urlpatterns = [
     path('user-list/', list_users, name='user_list'),
     path('manage-permissions/<int:user_id>', manage_permissions, name='manage_permissions'),
-    path('/', lambda request: render(request, 'home.html'), name='user_permissions'),
 ]
