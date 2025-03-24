19d889823c0141cf3415feee00e9e92f9d4572fc
root221 <panchenko.maxim87@gmail.com>
2025-03-24 12:47:44 +0200

    11 lesson 1:45:32

--- task_manager/accounts/admin.py
+++ task_manager/accounts/admin.py
@@ -1,10 +1,18 @@
+from distutils.command.clean import clean
+
 from django.contrib import admin
+from django import forms
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils import timezone
 from accounts.models import CustomUser, UserAddress, UserPayment
 
 # admin.site.register(CustomUser)
 # admin.site.register(UserAddress)
 admin.site.register(UserPayment)
+
+class DateInput(forms.DateInput):
+    input_type = 'data'
 
 class UserAddressInline(admin.StackedInline):
     model = UserAddress
@@ -17,6 +25,70 @@ class UserPaymentInLine(admin.TabularInline):
     fields = ('amount', 'payment_date', 'payment_method')
     readonly_fields = ('payment_date',)
 
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'phone_number', 'first_name', 'last_name', 'date_of_birth')
+        widgets = {'date_of_birth': DateInput()}
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and (password1 != password2):
+            raise forms.ValidationError("Passwords do not match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password1'))
+        if commit:
+            user.save()
+        return user
+
+class CustomUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(label='Current password', help_text='Encrypting version of password')
+    new_password1 = forms.CharField(
+        label='New_password',
+        widget=forms.PasswordInput,
+        required=False,
+        help_text="Leave blank if you don't want to change your password"
+    )
+    new_password2 = forms.CharField(
+        label='Confirm new password',
+        widget=forms.PasswordInput,
+        required=False
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        widgets = {
+            'date_of_birth': DateInput()
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password1 = cleaned_data.get('new_password1')
+        new_password2 = cleaned_data.get('new_password2')
+
+        if new_password1 or new_password2:
+            if new_password1 != new_password2:
+                raise forms.ValidationError('Passwords do not match')
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        new_password1 = self.cleaned_data.get("new_password1")
+        if new_password1:
+            user.set_password(new_password1)
+        if commit:
+            user.save()
+        return user
+
 
 @admin.register(UserAddress)
 class UserAddressAdmin(admin.ModelAdmin):
@@ -27,17 +99,26 @@ class UserAddressAdmin(admin.ModelAdmin):
 @admin.register(CustomUser)
 class CustomUserAdmin(BaseUserAdmin):
     inlines = [UserAddressInline, UserPaymentInLine]
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
     list_display = ('email', 'phone_number', 'is_active')
     list_filter = ('is_active', 'preferred_language')
     search_fields = ('email', 'phone_number', 'first_name', 'last_name')
     ordering = ('-date_joined',)
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        # ('Edit password', {'fields': ('new_password1', 'new_password2'), 'classes': ('collapse',)}),
+        (None, {'fields': ('email',)}),
+        ('Edit password', {'fields': ('new_password1', 'new_password2'), 'classes': ('collapse',)}),
         ('Personal data', {'fields': ('first_name', 'last_name', 'phone_number', 'date_of_birth', 'profile_of_picture'),
                            'classes': ('wide',)}),
 
 
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'phone_number', 'password1', 'password2', 'first_name', 'last_name', 'date_of_birth',)
+        }),
     )
     
