31de3b6d4d02da4893d1a8a137f53bd3f767f7a7
root221 <panchenko.maxim87@gmail.com>
2025-04-11 15:35:42 +0300

    15 lesson 1:27:41

--- task_manager/tasks_restfull/urls.py
+++ task_manager/tasks_restfull/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from tasks_restfull.views import TaskModelViewSet
+from tasks_restfull.views import TaskModelViewSet, TaskReadOnlyViewSet
 
 
 router = DefaultRouter()
 router.register(r'tasks', TaskModelViewSet)
+router.register(r'tasks-readonly', TaskReadOnlyViewSet, basename='tasks-readonly')
 
 urlpatterns = [
     path('', include(router.urls)),
--- task_manager/tasks_restfull/views.py
+++ task_manager/tasks_restfull/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets, filters, status
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from yaml import serialize
+
 from tasks.models import Task
 from tasks_restfull.serializers import TaskSerializer
 
@@ -14,3 +17,20 @@ class TaskModelViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save()
+
+
+class TaskReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["completed", "user"]
+
+
+class TaskCustomViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Task.objects.filter(completed=False)
+        serializer = TaskSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request):
+        pass
